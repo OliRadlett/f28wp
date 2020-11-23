8b329e6c38b6c5d79396bd80dc7b35d09091da44
Oli <o.radlett@gmail.com>
2020-11-23 18:08:01 +0000

    Add createAccount function
    Still need to generate an auth token on the server and store it as a cookie

--- game/js/menu.js
+++ game/js/menu.js
@@ -4,7 +4,7 @@ function login() {
     let password = document.getElementById("password").value;
     let url = "http://localhost:8081/login?username=" + username + "&password=" + password;
 
-    callApi(url, (data) => {
+    callApi("GET", url, (data) => {
 
         document.getElementById("debug").innerText = data;
 
@@ -12,11 +12,28 @@ function login() {
 
 }
 
-const callApi = async (url, callback) => {
+function createAccount() {
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    let url = "http://localhost:8081/create-account?username=" + username + "&password=" + password;
+
+    callApi("POST", url, (result) => {
+
+        alert(result);
+
+    });
+
+}
+
+const callApi = async (method, url, callback) => {
 
     try {
 
-        const response = await fetch(url);
+        const response = await fetch(url, {
+            method: method
+        });
 
         // const responseData = await response.json();
         const responseData = await response.text();
